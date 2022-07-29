ecefc4662fda7728ab5395bf83cfd6fbd52a9522
sandra smith <toosandra@gmail.com>
2022-07-29 18:02:05 -0400

    nav - defined a little of style and added href

--- myapp/src/components/Nav/index.js
+++ myapp/src/components/Nav/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
 
 function Nav() {
+    const linkStyle = { padding: '5px'};
 
     return (
-        <div>
+
+
+        <nav className="nav-header-menu">
+            <section style={{ 
+                display: 'flex',
+                fontFamily: 'helvetica',
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-evenly',
+            }}>
+                <div style={linkStyle}>
+                    <a href="#">Home</a>
+                </div>
+                <div style={linkStyle}>
+                    <a href="#">About</a>
+                </div>
+                <div style={linkStyle}>
+                    <a href="#">Work</a>
+                </div>
+                <div style={linkStyle}>
+                    <a href="#">Contact</a>
+                </div>
+                <div style={linkStyle}>
+                    <a href="../images/resume.pdf" target="_blank">Resume</a>
+                </div>
+
+
+            </section>
 
 
 
 
 
-        </div>
+        </nav>
     )
-    
+
 }
 
 export default Nav;
