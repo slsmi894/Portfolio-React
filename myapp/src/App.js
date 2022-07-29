a860a5217071de93c4376a61d38926859cbf52a5
sandra smith <toosandra@gmail.com>
2022-07-29 18:45:09 -0400

    updated app file, added Header

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,14 @@
 // import logo from './logo.svg';
 import React from 'react';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header>
         <h1>From App Js!</h1>
+
         {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +21,10 @@ function App() {
         >
           Learn React
         </a> */}
-      </header>
+      </Header>
+      <main>
+
+      </main>
     </div>
   );
 }
